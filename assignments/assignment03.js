cdede7ced12e1a44accc4d4dc3ef69e961600819
yungstupidlove <90069467+yungstupidlove@users.noreply.github.com>
2021-10-19 16:37:27 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -1,6 +1,6 @@
 let elem = [];
 // assign the entire table row for hole 1 to a variable, elem
-// elem[1] = document.getElementById("1");
+elem[1] = document.getElementById("1");
 
 // display the number of children (all td elements)
 // console.log(elem.children.length);
@@ -8,7 +8,9 @@ let elem = [];
 // console.log(elem.children[4].children[0]); 
 
 // assign a function to the + button
-// elem[1].children[4].children[0].onclick = function(){add1(elem[1]);};
+elem[1].children[4].children[0].onclick = function(){add1(elem[1]);};
+
+elem[1].children[4].children[1].onclick = function(){subtract1(elem[1]);};
 
 for(let i=1; i<=18; i++) {
   // console.log(i);
@@ -24,5 +26,16 @@ function add1 (elem) {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+
+// create an "subtract1" function
+function subtract1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "-1";
+  else {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
   }
 }
