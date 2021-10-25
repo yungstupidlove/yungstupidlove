bbc3a3172fb710beeb7118ddfd708823620e5912
yungstupidlove <90069467+yungstupidlove@users.noreply.github.com>
2021-10-25 17:03:25 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -1,18 +1,15 @@
 let elem = [];
+
 // assign the entire table row for hole 1 to a variable, elem
 elem[1] = document.getElementById("1");
-
-// display the number of children (all td elements)
-// console.log(elem.children.length);
-// display the content of the + button, which is the first child of the fifth element
-// console.log(elem.children[4].children[0]); 
 
 // assign a function to the + button
 elem[1].children[4].children[0].onclick = function(){add1(elem[1]);};
 
+// assign a function to the - button
 elem[1].children[4].children[1].onclick = function(){subtract1(elem[1]);};
 
-// assign a function to the - button
+// assign a function to the + button
 for(let i=1; i<=18; i++) {
   // console.log(i);
   elem[i] = document.getElementById(i.toString());
@@ -30,6 +27,7 @@ function add1 (elem) {
   }
 }
 
+// assign a function to the - button
 for(let i=1; i<=18; i++) {
   // console.log(i);
   elem[i] = document.getElementById(i.toString());
