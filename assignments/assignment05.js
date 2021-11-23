793883f4a3eb2e6b37e8996a958135431b3496ac
yungstupidlove <90069467+yungstupidlove@users.noreply.github.com>
2021-11-23 15:45:58 -0500

    Update assignment05.js

--- assignments/assignment05.js
+++ assignments/assignment05.js
@@ -109,7 +109,8 @@ function loadContent() {
         = newConfirmedOver1000.map( 
           (x) => x.NewDeaths );
       chartData.options.title.text 
-        = "Covid 19 Hotspots" ;
+        = "Covid 19 Hotspots" + " " + dayjs().format();
+  (dayjs().format("YYYY-MM-DD")); ;
       myChart = new Chart(ctx, chartData); 
 
     } // end if
